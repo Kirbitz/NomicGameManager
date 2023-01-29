3586f25243992a0a1f793e81a9ea1b2e7c126052
Tyler Marefke <tyler.marefke@gmail.com>
2023-01-29 12:31:30 -0600

    Created a basic todo app

--- app/src/main/java/manager/game/nomic/MainActivity.kt
+++ app/src/main/java/manager/game/nomic/MainActivity.kt
@@ -2,10 +2,45 @@ package manager.game.nomic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import manager.game.nomic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+
+        binding.rvTodoItems.layoutManager = layoutManager
+        binding.rvTodoItems.adapter = todoAdapter
+
+        binding.btnAddTodo.setOnClickListener {
+            val todoTitle = binding.etTodoTitle.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                binding.etTodoTitle.text.clear()
+            }
+        }
+
+        binding.btnDeleteTodo.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/manager/game/nomic/Todo.kt
@@ -0,0 +1,6 @@
+package manager.game.nomic
+
+data class Todo (
+    val title: String,
+    var isChecked: Boolean = false
+)
