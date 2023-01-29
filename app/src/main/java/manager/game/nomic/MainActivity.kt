0bdbed0bb5769ddf58b6c61ce1b93a0b89238b56
Tyler Marefke <tyler.marefke@gmail.com>
2023-01-28 20:07:03 -0600

    Adding Proper Files to Repo

---
+++ app/src/main/java/manager/game/nomic/MainActivity.kt
@@ -0,0 +1,11 @@
+package manager.game.nomic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
